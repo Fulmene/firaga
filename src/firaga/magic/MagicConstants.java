b32d32ba4c8b1644da14635c8d0c6a05b72f0f13
Ada Joule <ada.fulmina@gmail.com>
2018-01-17 15:10:45 +0700

    Add MagicConstants class

---
+++ src/firaga/magic/MagicConstants.java
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (C) 2018 Ada Joule
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package firaga.magic;
+
+public class MagicConstants {
+
+	public static final int MIN_DECK_SIZE = 60;
+	public static final int MAX_COPIES = 4;
+	public static final int MIN_SPELLS = 30;
+	public static final int MAX_SPELLS = 40;
+
+}
