055589dd734803a9cdf57e4e8cbc2c8f340f1df3
Ada Joule <ada.fulmina@gmail.com>
2018-05-04 14:06:57 +0700

    Upper fitness threshold

--- src/firaga/jenetics/DeckBuilderEngine.java
+++ src/firaga/jenetics/DeckBuilderEngine.java
@@ -152,7 +152,7 @@ public final class DeckBuilderEngine {
         if (level == this.engines.size() - 1)
             return this.engines.get(level).stream().limit(Limits.bySteadyFitness(10)).peek(this.saveDecks(level));
         else
-            return this.engines.get(level).stream().limit(Limits.byFitnessThreshold(7)).limit(50).peek(this.saveDecks(level));
+            return this.engines.get(level).stream().limit(Limits.byFitnessThreshold(15)).limit(50).peek(this.saveDecks(level));
     }
 
     public final Stream<EvolutionResult<IntegerGene, Integer>>
@@ -160,7 +160,7 @@ public final class DeckBuilderEngine {
         if (level == this.engines.size() - 1)
             return this.engines.get(level).stream(result).limit(Limits.bySteadyFitness(10)).peek(this.saveDecks(level));
         else
-            return this.engines.get(level).stream(result).limit(Limits.byFitnessThreshold(7)).limit(50).peek(this.saveDecks(level));
+            return this.engines.get(level).stream(result).limit(Limits.byFitnessThreshold(15)).limit(50).peek(this.saveDecks(level));
     }
 
     public final Stream<EvolutionResult<IntegerGene, Integer>>
@@ -168,7 +168,7 @@ public final class DeckBuilderEngine {
         if (level == this.engines.size() - 1)
             return this.engines.get(level).stream(population).limit(Limits.bySteadyFitness(10)).peek(this.saveDecks(level));
         else
-            return this.engines.get(level).stream(population).limit(Limits.byFitnessThreshold(7)).limit(50).peek(this.saveDecks(level));
+            return this.engines.get(level).stream(population).limit(Limits.byFitnessThreshold(15)).limit(50).peek(this.saveDecks(level));
     }
 
     public final List<MagicCardDefinition> getSpellPool() {
